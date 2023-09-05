ad9ddfafde83f9519b89de538e18912f8a068070
frankruns <franciscorrigan3@gmail.com>
2023-09-05 07:00:18 -0400

    Add unbeatable AI

--- static/script.js
+++ static/script.js
@@ -2,45 +2,136 @@ document.addEventListener('DOMContentLoaded', () => {
     const board = document.getElementById('board');
     const cells = document.querySelectorAll('.cell');
     const resetButton = document.getElementById('reset-button');
-    let currentPlayer = 'X';
+    const humanPlayer = 'X';
+    const aiPlayer = 'O';
+    let currentPlayer = humanPlayer;
+    let isGameOver = false;
 
     cells.forEach(cell => {
         cell.addEventListener('click', () => {
-            if (cell.textContent === '') {
-                cell.textContent = currentPlayer;
-                cell.setAttribute('data-cell', currentPlayer);
-                currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
-                document.querySelector('h2').textContent = `Player ${currentPlayer} Turn`;
-                checkWin();
+            if (!isGameOver && cell.textContent === '') {
+                cell.textContent = humanPlayer;
+                cell.setAttribute('data-cell', humanPlayer);
+
+                if (checkWin(humanPlayer)) {
+                    isGameOver = true;
+                    gameOver(humanPlayer);
+                } else if (!Array.from(cells).some(cell => cell.textContent === '')) {
+                    isGameOver = true;
+                    gameOver('draw');
+                } else {
+                    currentPlayer = aiPlayer;
+                    document.querySelector('h2').textContent = `Player ${currentPlayer} Turn`;
+
+                    setTimeout(() => {
+                        makeAIMove();
+                    }, 500); // 1 second delay after human player's move
+                }
             }
         });
     });
 
-    function checkWin() {
+    function makeAIMove() {
+        let bestScore = -Infinity;
+        let bestMoveIndex;
+
+        for (let i = 0; i < cells.length; i++) {
+            if (cells[i].textContent === '') {
+                cells[i].textContent = aiPlayer;
+                cells[i].setAttribute('data-cell', aiPlayer);
+
+                const score = minimax(cells, 0, false);
+
+                cells[i].textContent = '';
+                cells[i].setAttribute('data-cell', '');
+
+                if (score > bestScore) {
+                    bestScore = score;
+                    bestMoveIndex = i;
+                }
+            }
+        }
+
+        cells[bestMoveIndex].textContent = aiPlayer;
+        cells[bestMoveIndex].setAttribute('data-cell', aiPlayer);
+
+        if (checkWin(aiPlayer)) {
+            isGameOver = true;
+            gameOver(aiPlayer);
+        } else if (!Array.from(cells).some(cell => cell.textContent === '')) {
+            isGameOver = true;
+            gameOver('draw');
+        }
+
+        currentPlayer = humanPlayer;
+        document.querySelector('h2').textContent = `Player ${currentPlayer} Turn`;
+    }
+
+    function minimax(board, depth, isMaximizing) {
+        const scores = {
+            X: -1,
+            O: 1,
+            draw: 0
+        };
+
+        if (checkWin(humanPlayer)) {
+            return scores[humanPlayer];
+        } else if (checkWin(aiPlayer)) {
+            return scores[aiPlayer];
+        } else if (!Array.from(board).some(cell => cell.textContent === '')) {
+            return scores.draw;
+        }
+
+        if (isMaximizing) {
+            let bestScore = -Infinity;
+
+            for (let i = 0; i < board.length; i++) {
+                if (board[i].textContent === '') {
+                    board[i].textContent = aiPlayer;
+
+                    const score = minimax(board, depth + 1, false);
+
+                    board[i].textContent = '';
+
+                    bestScore = Math.max(score, bestScore);
+                }
+            }
+
+            return bestScore;
+        } else {
+            let bestScore = Infinity;
+
+            for (let i = 0; i < board.length; i++) {
+                if (board[i].textContent === '') {
+                    board[i].textContent = humanPlayer;
+
+                    const score = minimax(board, depth + 1, true);
+
+                    board[i].textContent = '';
+
+                    bestScore = Math.min(score, bestScore);
+                }
+            }
+
+            return bestScore;
+        }
+    }
+
+    function checkWin(player) {
         const winningCombinations = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
             [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
             [0, 4, 8], [2, 4, 6] // Diagonals
         ];
-    
-        let isGameOver = false;
-    
-        for (let i = 0; i < winningCombinations.length; i++) {
-            const [a, b, c] = winningCombinations[i];
-            const cellA = cells[a];
-            const cellB = cells[b];
-            const cellC = cells[c];
-    
-            if (cellA.textContent !== '' && cellA.textContent === cellB.textContent && cellA.textContent === cellC.textContent) {
-                isGameOver = true;
-                gameOver(cellA.textContent);
-                break;
-            }
-        }
-    
-        if (!isGameOver && Array.from(cells).every(cell => !!cell.textContent)) {
-            gameOver('draw');
-        }
+
+        return winningCombinations.some(combination => {
+            const [a, b, c] = combination;
+            return (
+                cells[a].textContent === player &&
+                cells[b].textContent === player &&
+                cells[c].textContent === player
+            );
+        });
     }
 
     function gameOver(result) {
@@ -60,8 +151,8 @@ document.addEventListener('DOMContentLoaded', () => {
             cell.textContent = '';
             cell.setAttribute('data-cell', '');
         });
-        currentPlayer = 'X';
+        currentPlayer = humanPlayer;
+        isGameOver = false;
         document.querySelector('h2').textContent = `Player ${currentPlayer} Turn`;
     });
 });
-
